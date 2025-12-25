b5b99bd25c8c06d86dd032c4b31713e551f49370
poorna36dev <poorna2429@gmail.com>
2025-12-25 15:30:33 +0530

    jwt , username and password , oauth security implemented

---
+++ src/main/java/com/poorna/JobTrackerApp/service/CustomerUserDetails.java
@@ -0,0 +1,22 @@
+package com.poorna.JobTrackerApp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.poorna.JobTrackerApp.repository.UserRepository;
+
+@Service
+public class CustomerUserDetails implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByEmail(username);
+    }
+
+}
---
+++ src/main/java/com/poorna/JobTrackerApp/util/JwtUtil.java
@@ -0,0 +1,50 @@
+package com.poorna.JobTrackerApp.util;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+    private final String secretKey = "my-secert-key-that-is-very-long-1234567890@#$%\"";
+    private final SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    private final long validityInMilliseconds = 3600000;
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + validityInMilliseconds))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        Claims user = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        return user.getSubject();
+    }
+
+    public boolean validateToken(String username, UserDetails userDetails, String token) {
+        if (username == userDetails.getUsername() && !isTokenExpired(token)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        Claims user = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        Date expiration = user.getExpiration();
+        return expiration.before(new Date());
+    }
+
+}
